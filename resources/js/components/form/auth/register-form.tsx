dd8b0d63a8e34d7358f53ab1944087c386be3ca1
Sina Shafiee <alisinashafieezada@gmail.com>
2025-03-28 06:37:14 +0330

    fix: register box size

--- resources/js/components/form/auth/register-form.tsx
+++ resources/js/components/form/auth/register-form.tsx
@@ -1,7 +1,7 @@
 export function RegisterForm() {
     console.log('register form');
     return (
-        <div className="mb-8 rounded-md border p-4">
+        <div className="mb-8 rounded-md border px-4 py-12">
             <p className="text-center">در حال حاضر امکان ثبت حساب کاربری جدید وجود ندارد</p>
         </div>
     );
