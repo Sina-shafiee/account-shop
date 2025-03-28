54e7fb554e6931afdee4e96ef8088d1e86c7a1d7
Sina Shafiee <alisinashafieezada@gmail.com>
2025-03-28 19:36:20 +0330

    fix: auth layout

--- resources/js/layouts/auth/auth-layout.tsx
+++ resources/js/layouts/auth/auth-layout.tsx
@@ -13,16 +13,16 @@ export default function AuthLayout({
     description?: string;
 }>) {
     return (
-        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-2 p-6 md:p-10">
+        <div className="md:bg-muted flex min-h-dvh flex-col items-center justify-center gap-2 sm:p-6 md:p-10">
             <Link href={route('home')} className="flex items-center gap-2 self-center font-medium">
                 <div className="flex h-9 w-9 items-center justify-center">
-                    <AppLogoIcon className="size-9 fill-current text-black dark:text-white" />
+                    <AppLogoIcon className="size-9 fill-current" />
                 </div>
             </Link>
-            <div className="mt-4 flex w-full max-w-md flex-col gap-2">
+            <div className="flex w-full max-w-md flex-col gap-2 sm:mt-4">
                 <div className="flex flex-col gap-6">
-                    <Card className="rounded-xl">
-                        <CardHeader className="px-10 pt-8 pb-0 text-center">
+                    <Card className="rounded-xl border-none py-4 md:border md:py-6">
+                        <CardHeader className="px-10 pb-0 text-center md:pt-8">
                             <CardTitle className="text-xl">{title}</CardTitle>
                             <CardDescription>{description}</CardDescription>
                         </CardHeader>
